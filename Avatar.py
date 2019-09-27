697e751e68f82b4a749140355164fcf550d9f22f
zqc970816 <1245910840@qq.com>
2019-09-27 12:51:12 +0800

    Added several frames

--- Avatar.py
+++ Avatar.py
@@ -2,20 +2,20 @@ from PIL import Image
 
 
 class Avatar(Image.Image):
-    def __init__(self, avatar_path, frame_path='AvatarFrame.png'):
+    def __init__(self, avatar_path):
         self.avatar = Image.open(avatar_path)
         self.avatar.convert('RGBA')
-        self.frame = Image.open(frame_path)
-        self.frame.convert('RGBA')
         self.frame_width_rate = 0.083
 
-    def GetNewAvatar(self):
-        frame_width = int(self.frame.size[0] * self.frame_width_rate)
-        re_size_w, re_size_h = self.frame.size[0] - frame_width, self.frame.size[1] - frame_width
+    def GetNewAvatar(self, mode='flag'):
+        frame = Image.open('AvatarFrame_{}.png'.format(mode))
+        frame.convert('RGBA')
+        frame_width = int(frame.size[0] * self.frame_width_rate)
+        re_size_w, re_size_h = frame.size[0] - frame_width, frame.size[1] - frame_width
         avatar = self.avatar.resize((re_size_w, re_size_h))
-        temp_avatar = Image.new('RGBA', self.frame.size)
+        temp_avatar = Image.new('RGBA', frame.size)
         x1 = int(frame_width / 2)
         y1 = int(frame_width / 2)
         temp_avatar.paste(avatar, (x1, y1, x1 + re_size_w, y1 + re_size_h))
-        temp_avatar.paste(self.frame, (0, 0, 2184, 2184), self.frame)
+        temp_avatar.paste(frame, (0, 0, 2184, 2184), frame)
         temp_avatar.save('new_avatar.png')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import Avatar
 
 avatar = Avatar.Avatar('Avatar.png')
-avatar.GetNewAvatar()
+avatar.GetNewAvatar('circleflag')
